d4deef55274223f50c9d0b323d1129046541913e
aaronscott072 <32349958+aaronscott072@users.noreply.github.com>
2023-09-12 17:07:56 +0200

    Fix enter chord

--- keyboards/splitkb/aurora/lily58/keymaps/aaron/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/aaron/keymap.c
@@ -18,7 +18,7 @@
 #define HOME_J     RCTL_T(KC_J)
 #define HOME_K     RSFT_T(KC_K)
 #define HOME_L     LALT_T(KC_L)
-#define HOME_SC    RGUI_T(KC_SCLN)
+#define HOME_SC    KC_SCLN        // RGUI_T(KC_SCLN)
 
 typedef enum {
     _LAYERS_HOME = 0,
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 	[_LAYERS_HOME] = LAYOUT(
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                      KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, 
         KC_ESC , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC, 
-        KC_LCTL, HOME_A , HOME_S , HOME_D , HOME_F , KC_G   ,                      KC_H   , HOME_J , HOME_K , HOME_L , KC_SCLN, KC_ENT , 
+        KC_LCTL, HOME_A , HOME_S , HOME_D , HOME_F , KC_G   ,                      KC_H   , HOME_J , HOME_K , HOME_L , HOME_SC, KC_ENT , 
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_MUTE,    KC_PSCR, KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_RSFT, 
                              KC_LGUI, KC_LALT, MO(1)  , LT(4, KC_SPC),                KC_SPC , MO(2)  , KC_DEL , KC_DEL ),
 	[_LAYERS_SYMBOLS] = LAYOUT(

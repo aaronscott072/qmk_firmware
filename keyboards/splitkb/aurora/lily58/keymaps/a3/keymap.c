dc08118208ed42416f7c17a2d078eaeb4e759af3
aaronscott072 <32349958+aaronscott072@users.noreply.github.com>
2023-12-21 16:45:55 +0100

    Add a ctrl to the right side thumbs for when working in word and wanting to hit Ctrl-I while using the mouse. Update LED symbols.t

--- keyboards/splitkb/aurora/lily58/keymaps/a3/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/a3/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                      KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSLS, 
         KC_LCTL, HOME_A , HOME_S , HOME_D , HOME_F , KC_G   ,                      KC_H   , HOME_J , HOME_K , HOME_L , HOME_SC, KC_ENT , 
         KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   , KC_MUTE,    KC_PSCR, KC_N   , KC_M   , TD_COMM, TD_DOT , TD_SLSH, KC_RSFT, 
-                            KC_RCTL, KC_LGUI, KC_LALT, OSM(MOD_LSFT),           KC_SPC , MO(1)  , KC_DEL , KC_DEL ),
+                            KC_RCTL, KC_LGUI, KC_LALT, OSM(MOD_LSFT),           KC_SPC , MO(1)  , KC_RCTL, KC_DEL ),
     [_LAYERS_2ND] = LAYOUT(
         _______, _______, _______, _______, _______, _______,                      _______, _______, _______, _______, _______, _______, 
         _______, KC_ESC , KC_TILD, KC_DQUO, KC_UNDS, KC_PPLS,                      KC_PGUP, KC_HOME, KC_END , KC_DEL , KC_BSPC, _______, 
@@ -143,10 +143,10 @@ bool oled_task_user(void)
 
     // Keys display
     switch (current_layer) {
-        case _LAYERS_HOME:          oled_write_P(is_left ? PSTR("12345qwertasdfgzxcvb   C^")  : PSTR("67890yuiophjkl;nm,./S1   "), false); break;
-        case _LAYERS_2ND:           oled_write_P(is_left ? PSTR("     e~\"_+t`'-=3{}[]   0^") : PSTR("     PHEDbLDURePLDR2S1   "), false); break;
-        case _LAYERS_NUM:           oled_write_P(is_left ? PSTR("     e MMMtMMMM3MMMM   0a")  : PSTR("+-*/ _789b.456e01232S1   "), false); break;
-        case _LAYERS_FN:            oled_write_P(is_left ? PSTR("FFFFpFFFF FFFFb3   b   0 ")  : PSTR(" vvv  mmm rrrr rrrr2 1   "), false); break;
+        case _LAYERS_HOME:          oled_write_P(is_left ? PSTR("12345qwertasdfgzxcvb CSA^")  : PSTR("67890yuiophjkl;nm,./S1CD "), false); break;
+        case _LAYERS_2ND:           oled_write_P(is_left ? PSTR("12345e~\"_+t`'-=3{}[] CS0^") : PSTR("67890PHEDbLDURePLDR2S0CD "), false); break;
+        case _LAYERS_NUM:           oled_write_P(is_left ? PSTR("12345e MMMtMMMM3MMMM CS0A")  : PSTR("+-*/ _789b.456e01232S1CD "), false); break;
+        case _LAYERS_FN:            oled_write_P(is_left ? PSTR("FFFFpFFFF FFFFb3   b CS0^")  : PSTR(" vvv  mmm rrrr rrrr2 1CD "), false); break;
         default:
             oled_write_P(PSTR("UNKNW"), false);
             break;
